e53b08d933366dc519cb03c2eb5c1a1c855d0f45
jmurillo-13 <61661469+jmurillo-13@users.noreply.github.com>
2020-07-04 12:10:40 -0400

    final push

---
+++ config.py
@@ -0,0 +1,12 @@
+sf_username = 'murillojessica12@gmail.com'
+sf_password = 'Blueberry1!'
+sf_security_token = '6mPs6eeMbL2gocPZWvURopGcz'
+
+remote_db_endpoint = 'hotmessjess.cua1jo4mglzx.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = 'HotMessJess1!'
+remote_db_name = 'gwsis'
+
+
+
